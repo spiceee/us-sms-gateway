3d31d1cc026e55a1f19d4a6318ac7fa494fb1e41
Fabio Mont Alegre <spiceee@gmail.com>
2024-02-22 17:57:20 -0300

    Add secure compare

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use redis::aio::ConnectionManager;
 use std::env;
 
 mod models {
+    use redis::aio::ConnectionManager;
     use serde::{Deserialize, Serialize};
 
     #[derive(Deserialize, Serialize, Clone, Debug)]
@@ -32,35 +33,61 @@ mod models {
         #[serde(rename = "NumSegments")]
         pub num_segments: Option<String>,
     }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct GlobalConfig {
+        pub token: Option<String>,
+    }
+
+    #[derive(Clone)]
+    pub struct AppState {
+        pub redis: ConnectionManager,
+        pub global_config: GlobalConfig,
+    }
 }
 
 mod handlers {
-    use crate::models::IncomingMessage;
+    use crate::models::{GlobalConfig, IncomingMessage};
+    use crate::AppState;
     use actix_web::middleware::Logger;
     use actix_web::{web, Error, HttpRequest, HttpResponse};
-    use redis::aio::ConnectionManager;
+    use orion::util::secure_cmp;
     use redis::AsyncCommands;
 
     pub async fn record_incoming_message(
         params: web::Form<IncomingMessage>,
-        redis_conn: web::Data<ConnectionManager>,
+        query: web::Query<GlobalConfig>,
+        config: web::Data<AppState>,
         _req: HttpRequest,
     ) -> Result<HttpResponse, Error> {
         Logger::new("New message {params:?}");
+        let mut validated = false;
+        let token = config.global_config.token.clone().expect("No token found");
 
-        let mut redis = redis_conn.get_ref().clone();
-        let _: () = redis
-            .set(
-                params.message_sid.clone(),
-                format!("From: {} Body: {}", params.from, params.body),
-            )
-            .await
-            .expect("Failed to write to Redis");
+        if let Some(token_from_query) = query.token.clone() {
+            validated = secure_cmp(token_from_query.clone().as_bytes(), token.as_bytes()).is_ok()
+        }
 
-        Ok(HttpResponse::Ok().content_type("text/xml").body(
-            "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message></Message></Response>"
-                .to_string(),
-        ))
+        match validated {
+            true => {
+                let mut redis = config.redis.clone();
+                let _: () = redis
+                    .set(
+                        params.message_sid.clone(),
+                        format!("From: {} Body: {}", params.from, params.body),
+                    )
+                    .await
+                    .expect("Failed to write to Redis");
+
+                Ok(HttpResponse::Ok().content_type("text/xml").body(
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message></Message></Response>"
+                        .to_string(),
+                ))
+            }
+            false => {
+                return Ok(HttpResponse::Unauthorized().finish());
+            }
+        }
     }
 
     pub async fn healthcheck(_req: HttpRequest) -> Result<HttpResponse, Error> {
@@ -74,6 +101,7 @@ async fn default_handler(_req_method: Method) -> Result<impl Responder> {
     Ok(HttpResponse::MethodNotAllowed().finish())
 }
 
+use crate::models::AppState;
 use handlers::{healthcheck, record_incoming_message};
 
 #[actix_web::main]
@@ -83,16 +111,23 @@ async fn main() -> std::io::Result<()> {
     let port = env::var("PORT").expect("Missing port number");
     let port = port.parse::<u16>().expect("Invalid port given");
 
-    let _token = env::var("PRIVATE_EXCHANGE_TOKEN").expect("Missing port number");
+    let secret_token = env::var("PRIVATE_EXCHANGE_TOKEN").expect("Missing port number");
     let redis_url = env::var("REDIS_PRIVATE_URL").expect("Missing Redis URL");
 
     let client = redis::Client::open(redis_url).unwrap();
     let backend = ConnectionManager::new(client).await.unwrap();
 
+    let data = web::Data::new(AppState {
+        redis: backend,
+        global_config: models::GlobalConfig {
+            token: Some(secret_token),
+        },
+    });
+
     let server = HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .app_data(web::Data::new(backend.clone()))
+            .app_data(data.clone())
             .service(web::resource("/incoming").route(web::post().to(record_incoming_message)))
             .service(web::resource("/healthcheck").route(web::get().to(healthcheck)))
             .default_service(web::to(default_handler))
