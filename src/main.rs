c06fffad3a4d5128dcb3af812bcd53ce4d7aefe3
Fabio Mont Alegre <spiceee@gmail.com>
2024-02-22 14:24:18 -0300

    Add serde config

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,106 @@
-#![allow(unused_imports, non_snake_case)] // refinery uses __ in migration filenames
+#![allow(unused_imports)]
+use actix_test::config;
+use actix_web::middleware::Logger;
 use actix_web::{
     get,
     http::{Method, StatusCode},
     web, App, Either, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
+use config::Config;
+use dotenvy::dotenv;
 use redis::aio::ConnectionManager;
 use redis::AsyncCommands;
+use std::env;
+
+mod models {
+    use chrono::{format::Numeric, DateTime, Utc};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct IncomingMessage {
+        #[serde(rename = "MessageSid")]
+        pub message_sid: Option<String>,
+        #[serde(rename = "SmsSid")]
+        pub sms_id: Option<String>,
+        #[serde(rename = "SmsMessageSid")]
+        pub sms_message_sid: Option<String>,
+        #[serde(rename = "AccountSid")]
+        pub account_sid: Option<String>,
+        #[serde(rename = "MessagingServiceSid")]
+        pub messaging_service_sid: Option<String>,
+        #[serde(rename = "From")]
+        pub from: String,
+        #[serde(rename = "To")]
+        pub to: String,
+        #[serde(rename = "Body")]
+        pub body: String,
+        #[serde(rename = "NumMedia")]
+        pub num_media: Option<String>,
+        #[serde(rename = "NumSegments")]
+        pub num_segments: Option<String>,
+    }
+}
+
+mod handlers {
+    use crate::models::IncomingMessage;
+    use actix_web::{web, Error, HttpRequest, HttpResponse};
+    use redis::Connection;
+
+    pub async fn record_incoming_message(
+        params: web::Form<IncomingMessage>,
+        _redis_conn: web::Data<Connection>,
+        _req: HttpRequest,
+    ) -> Result<HttpResponse, Error> {
+        println!("{params:?}");
+
+        Ok(HttpResponse::Ok()
+            .content_type("text/plain")
+            .body(format!("{:?}", params)))
+
+        // Ok(HttpResponse::Ok().into())
+    }
+
+    pub async fn welcome(_req: HttpRequest) -> Result<HttpResponse, Error> {
+        Ok(HttpResponse::Ok()
+            .content_type("text/plain")
+            .body(format!("Hello!",)))
+
+        // Ok(HttpResponse::Ok().into())
+    }
+}
+
+async fn default_handler(_req_method: Method) -> Result<impl Responder> {
+    Ok(HttpResponse::MethodNotAllowed().finish())
+}
+
+use handlers::{record_incoming_message, welcome};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let config_ = Config::builder()
-        .add_source(::config::Environment::default())
-        .build()
-        .unwrap();
+    let port = env::var("PORT").expect("Missing port number");
+    let port = port.parse::<u16>().expect("Invalid port given");
 
-    let config: ExampleConfig = config_.try_deserialize().unwrap();
-    let pool = config.pg.create_pool(None, NoTls).unwrap();
-
-    // Perform migrations
-    let mut conn = pool.get().await.unwrap();
-    let client = conn.deref_mut();
-
-    let token = env::var("PRIVATE_EXCHANGE_TOKEN").expect("Missing port number");
+    let _token = env::var("PRIVATE_EXCHANGE_TOKEN").expect("Missing port number");
     let redis_url = env::var("REDIS_PRIVATE_URL").expect("Missing Redis URL");
 
     let client = redis::Client::open(redis_url).unwrap();
-    let manager = ConnectionManager::new(client).await.unwrap();
-    let backend = RedisBackend::builder(manager).build();
 
     let server = HttpServer::new(move || {
+        let backend = client.get_connection().unwrap();
+
         App::new()
-            .app_data(web::Data::new(backend.clone()))
-            .service(web::resource("/posts.json").route(web::post().to(add_tracking_code)))
+            .wrap(Logger::default())
+            .app_data(web::Data::new(backend))
+            .service(web::resource("/incoming").route(web::post().to(record_incoming_message)))
+            .service(web::resource("/test").route(web::get().to(welcome)))
             .default_service(web::to(default_handler))
     })
     .bind(("0.0.0.0", port))?
     .run();
 
-    println!("Server running at http://{}/", config.server_addr);
+    println!("Server running at http://0.0.0.0:{}/", port);
 
     server.await
 }
